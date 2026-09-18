de3a531dabc6f99e74aca4bf3e02c6f274ce700d
NikitirolYT <prechtl.niklas@schlosswagrain.at>
2026-09-18 15:45:37 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,19 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "status": "online",
+        "message": "Sister Community Server läuft! 🚀"
+    })
+
+@app.route("/api/status")
+def status():
+    return jsonify({
+        "online": True
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
